4d9729b624f14d8c88f2c07cd9a6e65253a138cf
alievaleila <meliyeva452@gmail.com>
2026-03-01 23:30:03 +0400

    feat: implement custom ProductNotFoundException for better error handling

---
+++ src/main/java/com/dailycodework/dream_shops/exceptions/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package com.dailycodework.dream_shops.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
+        super(message);
+
+    }
+}
